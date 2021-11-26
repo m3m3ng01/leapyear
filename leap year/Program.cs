95d430bec4c8c25167d34262765936c082dc953a
m3m3ng01 <antarsayuridn@gmail.com>
2021-11-26 18:39:35 +0700

    leapyear

---
+++ leap year/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leap_year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukan tahun awal: ");
+            int pertama = int.Parse(Console.ReadLine());
+            Console.Write("Masukan tahun akhir: ");
+            int terakhir = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = pertama+1; i <= terakhir; i++)
+            {
+                sum = sum + i;
+                if (i % 4 == 0)
+                {
+                    Console.Write(i + ", ");         
+                } 
+            }
+            Console.ReadLine();
+        }
+    }
+}
